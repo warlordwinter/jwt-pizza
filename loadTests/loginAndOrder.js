5fc07428abc6045441b8b8695def7c0f62166062
aonstott <aonstott32@gmail.com>
2025-03-27 15:46:28 -0600

    hi

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,195 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+
+const vars = {};
+
+export const options = {
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  group("page_1 - https://pizza.aonstott329.click/", function () {
+    // Home
+    response = http.get("https://pizza.aonstott329.click/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Sun, 09 Mar 2025 01:09:53 GMT",
+        "if-none-match": '"a6ee7ed9ababece2bfbee6388519a97a"',
+        priority: "u=0, i",
+        "sec-ch-ua":
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+    });
+    sleep(12.1);
+
+    // Login
+    response = http.put(
+      "https://pizza-service.aonstott329.click/api/auth",
+      '{"email":"a@jwt.com","password":"admin"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.aonstott329.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    vars.authToken = response.json().token;
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+    sleep(7.7);
+
+    // Get Menu
+    response = http.get(
+      "https://pizza-service.aonstott329.click/api/order/menu",
+      {
+        headers: {
+          accept: "*/*",
+          authorization: `Bearer ${vars.authToken}`,
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          "if-none-match": 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+          origin: "https://pizza.aonstott329.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    // Get Franchise
+    response = http.get(
+      "https://pizza-service.aonstott329.click/api/franchise",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          authorization: `Bearer ${vars.authToken}`,
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          "if-none-match": 'W/"98-QfG3h23fvKkBXOwhLjB1uFtx8LE"',
+          origin: "https://pizza.aonstott329.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    sleep(18.6);
+
+    // Buy Pizza
+    response = http.post(
+      "https://pizza-service.aonstott329.click/api/order",
+      '{"items":[{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          authorization: `Bearer ${vars.authToken}`,
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.aonstott329.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    vars.pizzaToVerify = response.json().jwt;
+    sleep(1.7);
+
+    // Verify
+    const payload = JSON.stringify({ jwt: vars.pizzaToVerify });
+    response = http.post(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      payload,
+      {
+        headers: {
+          accept: "*/*",
+          authorization: `Bearer ${vars.authToken}`,
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.aonstott329.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "sec-fetch-storage-access": "active",
+        },
+      }
+    );
+
+    console.log(response.json);
+  });
+}
